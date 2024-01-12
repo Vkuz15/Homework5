10efae9ae0f97571541ac5f2326b4d49ee1c8b4e
Владимир <vkuznecov1504@mail.ru>
2024-01-12 13:32:42 +0400

    Инициализировали переменную year добавили условный оператор if-else для определения високосного года

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,14 @@ public class Main {
                 System.out.println("Установите облегченную версию приложения для Android по ссылке");
             }
         }
+
+        // Задача 3
+
+        int year = 2024;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
     }
 }
