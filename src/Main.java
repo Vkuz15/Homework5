63a99b595f041ea0b8aae2922401d31b6ea228cf
Владимир <vkuznecov1504@mail.ru>
2024-01-12 12:14:04 +0400

    Инициализировали переменную clientOS Добавили условный оператор if-else

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+
+        // Задача 1
+
+        int clientOS = 1;
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
     }
 }
