f0a23ca73024bab24e58404c259c00a00422783e
Владимир <vkuznecov1504@mail.ru>
2024-01-12 12:28:44 +0400

    Инициализировали переменную clientOS Инициализировали переменную clientDeviceYear Добавили комбинацию условий if-else

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Задача 1
+        // Задача 2
 
-        int clientOS = 1;
+        int clientOS = 0;
+        int clientDeviceYear = 2014;
         if (clientOS == 1) {
             System.out.println("Установите версию приложения для iOS по ссылке");
+            if (clientDeviceYear <= 2014) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
         } else {
             System.out.println("Установите версию приложения для Android по ссылке");
+            if (clientDeviceYear <= 2014) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
         }
     }
 }
